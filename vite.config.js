867aa463b58e2abcc5f59a46fd80925f8a9ac0eb
mahasaya <83829233+mahasaya@users.noreply.github.com>
2023-10-09 20:09:11 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"./Course-React",
+  base:"/Course-React/",
   plugins: [react()],
 })
